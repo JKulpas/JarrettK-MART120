59764726301fa117aca925a97dd59999a979f482
JKulpas <112431920+JKulpas@users.noreply.github.com>
2022-10-31 19:30:22 -0600

    Add files via upload

---
+++ Homework 9/js/sketch.js
@@ -0,0 +1,54 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    textSize(22)
+      text("Picture Portrait",0,25)
+    
+    strokeWeight(10);
+    fill(255,0 ,0)
+    rect(115, 160, 150)
+    
+    fill(255,160,100);
+      circle(190,100,120)
+    triangle(170,160,205,160,190,180)
+    strokeWeight(4);
+    line(190,105,190,110)
+    line(150,80,230,80)
+    line(190,80,190,100)
+    line(150,100,230,100)
+    line(150,100,150,80)
+    line(230,100,230,80)
+    line(230,80,250,70)
+    line(150,80,130,70)
+    fill(160,82,45)
+    ellipse(190,55,100,35)
+    fill(255,0,0)
+    ellipse
+    
+   strokeWeight(10);
+      fill(0);
+      point(165,90);
+      point(215,90);
+    
+    strokeWeight(10);
+    ellipse(190,135,25,10)
+    fill(255,0,0)
+    rect(90,160,30,150)
+    rect(260,160,30,150)
+    
+    fill(0,0,255)
+    rect(130,320,50,150)
+    rect(200,320,50,150)
+    strokeWeight(10);
+    strokeWeight(2);
+    fill(255,0,0)
+    ellipse(190,140,25,10)
+    strokeWeight(10);
+     fill(0)
+      textSize(22);
+       text("Jarrett Kulpas",260,375)
+  
+  }
